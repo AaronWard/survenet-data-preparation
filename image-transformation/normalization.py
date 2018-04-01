8d979e00802fa296dd5b7559e1870e58850ff8e2
AaronWard <aaronlaptop12@hotmail.com>
2018-04-01 21:22:01 +0100

    haha

--- image-transformation/normalization.py
+++ image-transformation/normalization.py
@@ -1,25 +1,20 @@
 from PIL import Image
 import numpy as np
 
-def noramlize(file_name):
-    #
-    #
-    #
-    #
-
-
+def normalize(file_name):
+    'C:/Users/aaron/Desktop/data-preparation/flipped_S005_001_00000005_cropped_.png'
 
 
 #Load the directory and traverse over all the image files
-list = os.listdir('C:/Users/aaron/Desktop/Cropped Dataset/suprise/')
-for file in list:
+# list = os.listdir('C:/Users/aaron/Desktop/Cropped Dataset/suprise/')
+# for file in list:
 
-    file_n = file
-    print(file_n)
+#     file_n = file
+#     print(file_n)
 
-    #error with PIL occurs so this is used to prevent
-    #an OSError
-    if(file_n == "desktop.ini"):
-        print("AHHHH")
-    else:
-        flip_image(file_n)
+#     #error with PIL occurs so this is used to prevent
+#     #an OSError
+#     if(file_n == "desktop.ini"):
+#         print("AHHHH")
+#     else:
+#         flip_image(file_n)
