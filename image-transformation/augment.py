9602f0235ba2be7600491de782f3b5ef6570b72e
AaronWard <aaronlaptop12@hotmail.com>
2018-04-04 17:52:43 +0100

    used on test

--- image-transformation/augment.py
+++ image-transformation/augment.py
@@ -8,31 +8,32 @@ This script is used for augmenting images slightly
 
 '''
 # Augment images for angry
-p = Augmentor.Pipeline('C:/Users/aaron/Desktop/Cropped Dataset/angry')
+p = Augmentor.Pipeline('C:/Users/aaron/Desktop/model-training-tensorflow/data/testing/angry')
 p.random_distortion(probability=1, grid_width=4, grid_height=4, magnitude=7)
-p.sample(609)
-
-# Augment images for happy
-p = Augmentor.Pipeline('C:/Users/aaron/Desktop/Cropped Dataset/happy')
-p.random_distortion(probability=1, grid_width=4, grid_height=4, magnitude=7)
-p.sample(81)
-
-# Augment images for neutral
-p = Augmentor.Pipeline('C:/Users/aaron/Desktop/Cropped Dataset/neutral')
-p.random_distortion(probability=1, grid_width=4, grid_height=4, magnitude=7)
-p.sample(358)
-
-# Augment images for sadness
-p = Augmentor.Pipeline('C:/Users/aaron/Desktop/Cropped Dataset/sadness')
-p.random_distortion(probability=1, grid_width=4, grid_height=4, magnitude=7)
-p.sample(541)
-
-# Augment images for suprise
-p = Augmentor.Pipeline('C:/Users/aaron/Desktop/Cropped Dataset/suprise')
-p.random_distortion(probability=1, grid_width=4, grid_height=4, magnitude=7)
-p.sample(445)
+p.sample(152)
 
 # Augment images for fear
-p = Augmentor.Pipeline('C:/Users/aaron/Desktop/Cropped Dataset/fear')
+p = Augmentor.Pipeline('C:/Users/aaron/Desktop/model-training-tensorflow/data/testing/fear')
 p.random_distortion(probability=1, grid_width=4, grid_height=4, magnitude=7)
-p.sample(784)
+p.sample(196)
+
+
+# Augment images for happy
+p = Augmentor.Pipeline('C:/Users/aaron/Desktop/model-training-tensorflow/data/testing/happy')
+p.random_distortion(probability=1, grid_width=4, grid_height=4, magnitude=7)
+p.sample(21)
+
+# Augment images for neutral
+p = Augmentor.Pipeline('C:/Users/aaron/Desktop/model-training-tensorflow/data/testing/neutral')
+p.random_distortion(probability=1, grid_width=4, grid_height=4, magnitude=7)
+p.sample(90)
+
+# Augment images for sadness
+p = Augmentor.Pipeline('C:/Users/aaron/Desktop/model-training-tensorflow/data/testing/sadness')
+p.random_distortion(probability=1, grid_width=4, grid_height=4, magnitude=7)
+p.sample(135)
+
+# Augment images for suprise
+p = Augmentor.Pipeline('C:/Users/aaron/Desktop/model-training-tensorflow/data/testing/suprise')
+p.random_distortion(probability=1, grid_width=4, grid_height=4, magnitude=7)
+p.sample(111)
